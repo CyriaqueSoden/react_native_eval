7f7b82c72ba404549ddcc6a0b19e00310ebb2c7f
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-10 16:22:56 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,135 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TextInput, Button, SafeAreaView, TouchableOpacity, Switch } from 'react-native';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState, useEffect } from 'react';
 
-export default function App() {
+
+
+
+
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+function ScreenBundlePersonnage({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="SearchPersonnage" component={SearchPersonnageScreen} />
+      <Stack.Screen name="ListePersonnage" component={ListePersonnageScreen} />
+      <Stack.Screen name="ImagePersonnage" component={ImagePersonnageScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ViewedScreen({ navigation }) {
+
+  const ALL_EPISODES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={ALL_EPISODES}
+        renderItem={({ item }) => {
+          <View>
+            <TouchableOpacity>{item}</TouchableOpacity>
+          </View>
+
+        }
+        }
+      />
+    </SafeAreaView>
+  )
+
+
+}
+
+function SearchPersonnageScreen({ navigation }) {
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <View>
+      <TextInput
+        type="text" style={{ borderWidth: 1 }}
+      />
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity style={{ marginTop: 15 }} onPress={toggleSwitch}>
+          <Text>Uniquement les personnages morts
+          </Text>
+        </TouchableOpacity>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <Button title='Search' onPress={() => navigation.navigate('ListePersonnage')} />
+
+
+    </View>
+  )
+}
+function ListePersonnageScreen({ navigation }) {
+
+  const [personnage, setPersonnage] = useState()
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/').then((response) => {
+      return response.json()
+    }).then((json) => {
+      setPersonnage(json)
+    })
+  }, [])
+
+
+  return (
+    <View>
+      {/* doomed by the FlatList */}
+      <FlatList
+        data={personnage}
+        renderItem={({ item }) => {
+
+          <Text>{item.image}</Text>
+
+        }
+        }
+      />
+      <Button title="c'est une image" onPress={() => navigation.navigate('ImagePersonnage')} />
+    </View>
+  )
+}
+
+
+function ImagePersonnageScreen({ navigation }) {
+  return (
+    <View>
+      <Button title="Retour a la search bar" onPress={() => navigation.navigate('SearchPersonnage')} />
+    </View>
+  )
+}
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Recherche" component={ScreenBundlePersonnage} options={{ headerShown: false }} />
+        <Tab.Screen name="Vue" component={ViewedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+
+export default App;
